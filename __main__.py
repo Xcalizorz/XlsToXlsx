2d167972dc3ce6f12817d2719b5c466ac6e0f02f
Zadjad Rezai <38798281+zjk758@users.noreply.github.com>
2018-08-11 21:22:03 +0200

    Small Changes, deleted Electron stuff, until next release

---
+++ __main__.py
@@ -0,0 +1,29 @@
+from FileManipulator import FileManipulator
+from tkinter.filedialog import askdirectory
+import os.path
+
+
+def __main__():
+
+    # Path declaration block | init. directory is the directory of this python script
+    input_path = os.path.normpath(
+        askdirectory(initialdir = os.path.normpath(os.path.dirname(__file__))
+        )
+    )
+    output_path = os.path.normpath(input_path + "_new")
+
+    # Converting block
+    converter = FileManipulator(input_path, output_path)
+    
+    # creates a list of paths to the .xls files
+    xls_files = converter.find_files("(.*?).xls$")
+
+    # creating the copy
+    if xls_files:
+        converter.copy_directory(input_path, output_path)
+        converter.xls_to_xlsx(xls_files)
+    else:
+        print("No .xls files found!")
+
+if __name__ == "__main__":
+    __main__()
--- engine/boot.sh
+++
@@ -1,8 +0,0 @@
-kill -9 $(lsof -t -i:5000)
-echo killed pre
-python __main__.py &
-cd ..
-cd gui
-npm start
-kill -9 $(lsof -t -i:5000)
-echo killed
